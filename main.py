1248913594b70f3969ebc84648da36f72c1fe861
Antônio Gabriel Follone Graça <gabrielfollone27@gmail.com>
2024-12-03 21:37:15 -0300

    Added message on start code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from get_pagecontent import get_pagecontent
 # Agende o script para rodar a cada 1 minuto
 schedule.every(1).hour.do(get_pagecontent)
 
+# Print só pra indicar que o código iniciou
+print('Start')
+
 # Mantém o programa rodando para verificar os agendamentos
 while True:
     schedule.run_pending()
